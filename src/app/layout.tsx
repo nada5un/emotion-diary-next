39e6239b68b2fe0206fa9524fa152ff5eb469750
daeun <sung980524@naver.com>
2025-06-16 22:23:46 +0900

    update: replace font with Noto Sans KR and simplify layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
+const notoSansKR = Noto_Sans_KR({
+    variable: "--font-noto-sans-kr",
     subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+    weight: ["400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                {children}
-            </body>
+            <body className={`${notoSansKR.variable} `}>{children}</body>
         </html>
     );
 }
